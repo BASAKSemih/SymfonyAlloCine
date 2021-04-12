1610ec4fe41af8d8e74a030132fc85da433e086e
BASAK Semih <semihbasak25@gmail.com>
2021-04-12 17:01:37 +0200

    adding register template, form

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegisterController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/inscription", name="register")
+     */
+    public function index(): Response
+    {
+
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+
+
+            return $this->render('register/index.html.twig', [
+                'form' => $form->createView()
+            ]);
+        }
+}
+
+
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,17 +4,49 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
+            ->add('email', EmailType::class, [
+                'label' => false,
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 125
+                ]),
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre email'
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passes doivent être identiques',
+                'required' => true,
+                'first_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'placeholder' => 'Merci de saisir votre mot de passe.'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'placeholder' => 'Merci 2de confirmer votre mot de passe.'
+                    ]
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscrire"
+            ])
         ;
     }
 
